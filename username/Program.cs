48c38f70f43aaab28f20a7577524de76c0f1d66b
vjlion <89218741998@mail.ru>
2022-12-13 21:51:37 +0300

    add description for the line

--- username/Program.cs
+++ username/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine(); //спросить( после чего появится первая строчка + 3-я)
 Console.Write("Привет, ");
 Console.WriteLine(username);
