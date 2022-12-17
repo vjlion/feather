615676d31d0a9ff1b344aa0700ab67d0672b16b1
vjlion <89218741998@mail.ru>
2022-12-17 19:03:40 +0300

    Numbers_random

---
+++ Numbers_random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.Write("Сумма: ");
+Console.WriteLine(numberA + numberB);
