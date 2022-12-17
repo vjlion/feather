d46bc0ff9d76a218e0213a525ee7126f4d44e744
vjlion <89218741998@mail.ru>
2022-12-17 14:12:12 +0300

    как поприветствовать пользователя любимчика?

---
+++ nurseling/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine()!;
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("Я так ждал тебя, Маша!");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username);
+}
