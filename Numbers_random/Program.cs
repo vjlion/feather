7f8016749b28b4e46f57ee47511815e4a3f109ab
vjlion <89218741998@mail.ru>
2022-12-17 19:05:53 +0300

    line numbering

--- Numbers_random/Program.cs
+++ Numbers_random/Program.cs
@@ -1,6 +1,8 @@
 ﻿int numberA = new Random().Next(1, 10);
+Console.Write("1): ");
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
+Console.Write("2): ");
 Console.WriteLine(numberB);
 Console.Write("Сумма: ");
 Console.WriteLine(numberA + numberB);
